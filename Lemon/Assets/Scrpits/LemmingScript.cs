8559ee4b17f495ace1bc3a64e9d1a6ff2a449615
Joshua Chan <chanj313@myumanitoba.ca>
2015-09-08 22:23:20 -0500

    ClickNDrag,misc improvements

---
+++ Lemon/Assets/Scrpits/LemmingScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LemmingScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MoveForward();
+	}
+
+	/*
+	 * Automatically move forward.
+	 * 
+	 * TODO Only move when "feet" are on some kind of "ground"?
+	 */
+	void MoveForward(){
+		transform.Translate(Vector3.right * Time.deltaTime);
+	}
+}
